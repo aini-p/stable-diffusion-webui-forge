8953cac5b1f0d72564d7ed62fea15baafdc61c55
aini-p <aini.h.panda@gmail.com>
2026-01-20 15:22:51 +0900

    multi gpuサンプル

---
+++ webui-user-gpu0.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set CUDA_VISIBLE_DEVICES=0
+set COMMANDLINE_ARGS=--device-id 0 --port 9999 --lora-dir "E:/AI/lora" --embeddings-dir "E:/AI/embeddings" --ckpt-dir "E:/AI/models"
+
+@REM Uncomment following code to reference an existing A1111 checkout.
+@REM set A1111_HOME=Your A1111 checkout dir
+@REM
+@REM set VENV_DIR=%A1111_HOME%/venv
+@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
+@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
+@REM  --embeddings-dir %A1111_HOME%/embeddings ^
+@REM  --lora-dir %A1111_HOME%/models/Lora
+
+call webui.bat
